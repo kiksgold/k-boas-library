9c64df884767d05a9f32d6b9a2efeb6dbd01ebd0
kiksgold <kiksgold@gmail.com>
2022-11-14 11:57:25 +0000

    Install django-summernote

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Book
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
