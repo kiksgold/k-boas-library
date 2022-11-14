4a8328468dfb40d1455f5e6344608c7bce5c60fd
kiksgold <kiksgold@gmail.com>
2022-11-14 08:59:44 +0000

    Attached new database

--- boaslibrary/settings.py
+++ boaslibrary/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['k-boas-library.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['new-library-app20.herokuapp.com', 'localhost']
 
 
 # Application definition
