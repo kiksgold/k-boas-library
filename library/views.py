cbec9b08164e5b0f9d567ebcfce26218a74151e6
kiksgold <kiksgold@gmail.com>
2022-11-14 12:41:12 +0000

    Create templates and static folders

--- library/views.py
+++ library/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Book
 
-# Create your views here.
+
+class BookList(generic.BookView):
+    model = Book
+    queryset = Book.objects.filter(status=1).order_by('-updated_on')
+    template_name = 'index.html'
+    paginate_by = 4
